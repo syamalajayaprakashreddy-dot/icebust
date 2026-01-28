eba7bcabdcdfab92cc964d471c8c4f1f96d2d491
JayaPrakash Reddy <jayaprakashreddy@JayaPrakashsMBP.lan>
2026-01-28 23:39:39 +0000

    fix render deployment config

--- my/settings.py
+++ my/settings.py
@@ -147,8 +147,8 @@ EMAIL_USE_TLS = True
 
 
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("justuse0863@gmail.com")
+EMAIL_HOST_PASSWORD = os.environ.get("King@123")
 
 
 DEFAULT_FROM_EMAIL = "noreply@icebust.shop"

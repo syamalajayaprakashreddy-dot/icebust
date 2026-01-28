ef5e017469d894a205587290c40e055475a6618b
JayaPrakash Reddy <jayaprakashreddy@JayaPrakashsMBP.lan>
2026-01-28 20:27:30 +0000

    add whitenoise and fix static files

--- my/settings.py
+++ my/settings.py
@@ -59,9 +59,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    
-
 ]
+
 
 ROOT_URLCONF = "my.urls"
 
